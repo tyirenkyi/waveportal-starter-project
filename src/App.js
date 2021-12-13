a5c90d90eadcf5fa83f784936d8c51fe13afaa46
Timothy Odei Yirenkyi <t.o.yirenkyi@gmail.com>
2021-12-13 00:14:39 +0000

    install tailwind and change app title

--- src/App.js
+++ src/App.js
@@ -17,11 +17,11 @@ export default function App() {
         </div>
 
         <div className="bio">
-        I am darth_applejeewce. I'm a frontend developer. Connect your Ethereum wallet and wave at me!
+        I am darth_applejeewce. I'm a frontend developer. Connect your Ethereum wallet and high five me!
         </div>
 
-        <button className="waveButton" onClick={wave}>
-          Wave at Me
+        <button className="bg-blue-100 w-[200px] mx-auto mt-4 py-4 rounded-md font-semibold text-[18px]" onClick={wave}>
+          High Five Me
         </button>
       </div>
     </div>
